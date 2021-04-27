e620d4f21ac9daa0cff51a50ff90f50066d910f9
dmariel <34478937+dmariel@users.noreply.github.com>
2021-04-27 19:02:22 +0200

    fix: Test

--- secure-api-endpoint-using-keycloak/foreground.sh
+++ secure-api-endpoint-using-keycloak/foreground.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-    if [[ `lsof | grep hedgehog.url` ]]
+    if [[ `lsof | grep hedgehog.jpeg` ]]
     then
         vim -o hedgehog.jpeg
         curl https://64.media.tumblr.com/3487d11245609f74cb81c7e67c49bac5/tumblr_npnnnwNWJq1qc4uvwo1_500.jpg | display
