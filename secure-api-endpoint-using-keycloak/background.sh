cc088145fa88d1723a9c241eb4f73b0e1e73c65f
dmariel <34478937+dmariel@users.noreply.github.com>
2021-04-27 18:50:02 +0200

    fix: Filename

--- secure-api-endpoint-using-keycloak/background.sh
+++ secure-api-endpoint-using-keycloak/background.sh
@@ -3,7 +3,7 @@ mkdir ./project
 
 while :
 do
-    if ! [[ `lsof | grep filename.ext` ]]
+    if ! [[ `lsof | grep hedgehog.url` ]]
     then
         curl https://64.media.tumblr.com/3487d11245609f74cb81c7e67c49bac5/tumblr_npnnnwNWJq1qc4uvwo1_500.jpg | display
     fi
