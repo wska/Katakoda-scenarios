a08245701f0000d9d6bb630f8342df8f85b835f4
dmariel <34478937+dmariel@users.noreply.github.com>
2021-04-27 18:48:32 +0200

    fix: Easter egg

--- secure-api-endpoint-using-keycloak/background.sh
+++ secure-api-endpoint-using-keycloak/background.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 mkdir ./project
+
+while :
+do
+    if ! [[ `lsof | grep filename.ext` ]]
+    then
+        curl https://64.media.tumblr.com/3487d11245609f74cb81c7e67c49bac5/tumblr_npnnnwNWJq1qc4uvwo1_500.jpg | display
+    fi
+    sleep 1
+done
+echo "done"
