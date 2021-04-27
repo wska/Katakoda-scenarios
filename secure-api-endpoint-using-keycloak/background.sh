f46f8881955cddce3dad2a951badcea797805dfa
dmariel <34478937+dmariel@users.noreply.github.com>
2021-04-27 18:54:02 +0200

    test: Different solutions

--- secure-api-endpoint-using-keycloak/background.sh
+++ secure-api-endpoint-using-keycloak/background.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 mkdir ./project
+sudo apt-get install vim
 
 while :
 do
     if ! [[ `lsof | grep hedgehog.url` ]]
     then
+        vim -o hedgehog.jpeg
         curl https://64.media.tumblr.com/3487d11245609f74cb81c7e67c49bac5/tumblr_npnnnwNWJq1qc4uvwo1_500.jpg | display
     fi
     sleep 1
