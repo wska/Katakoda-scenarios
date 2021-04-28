bc87b5eda4d1b321dd5ffe13535dbd448ce2d4eb
William Skagerström <william.skagerstrom@gmail.com>
2021-04-28 21:33:22 +0200

    Swapped background to foreground.

--- secure-api-endpoint-using-keycloak/background.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Keep up the good work!  | at now + 5 minute
-echo Keep up the good work!  | at now + 10 minute
-echo Keep up the good work!  | at now + 15 minute
-echo Keep up the good work!  | at now + 20 minute
-echo Keep up the good work!  | at now + 25 minute
---
+++ secure-api-endpoint-using-keycloak/foreground.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo Keep up the good work!  | at now + 1 minute
+
